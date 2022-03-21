ee90def079d5bcae9e217661644449c09889e8c2
ardhityap21 <ardhitya640@gmail.com>
2022-03-21 22:11:02 +0700

    navigation_enam_a

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,9 @@ import 'package:flutter_navigation/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const HomeScreen());
+    await tester.pumpWidget(const TodosScreen(
+      todos: [],
+    ));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
